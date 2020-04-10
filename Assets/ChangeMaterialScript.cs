d35dd978c51ac80c22ee26bf6ae5867f42fc4c6f
Adrianfrizberg <Adrian.frizberg@gmail.com>
2020-04-10 16:29:28 +0200

    new World,VR Setup, Teleportation, Textures, Interaction,

---
+++ Assets/ChangeMaterialScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ChangeMaterialScript : MonoBehaviour
+{
+    public Material[] material;
+    public MeshRenderer rend;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend.sharedMaterial = material[0];
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.collider.CompareTag("screwable"))
+        {
+            rend.sharedMaterial = material[1];
+        }
+    }
+}
